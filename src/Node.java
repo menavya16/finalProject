7d267c8468602a85d339a6ebccdf3c0828ed14df
asalatk <kamala6@mcmaster.ca>
2017-03-24 10:04:01 -0400

    Node.java creates a 'Node' using a unique Key, a female input and a MALE* input

--- src/Node.java
+++ src/Node.java
@@ -3,9 +3,11 @@ import java.util.ArrayList;
 // A node of chains
 class Node<K, V1, V2>
 {
-	K key;
-	V1 value1;
-	V2 value2;
+	
+	//these are set to public so they can easily be accessed in the hashtable.java
+	public K key;
+	public V1 value1;
+	public V2 value2;
 
 	// Reference to next node
 	Node<K, V1, V2> next;
